094cfa5d72ee18321d638c01be3ab9d97b98f127
GalleryBridge <laidoring@163.com>
2024-12-05 23:50:33 +0800

    feat: index paeg

---
+++ components/theme-item/theme-item.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="themeItem">
+		<navigator url="#" class="box" v-if="!isMore">
+			<image class="pic" src="../../common/image/swapper2.png" mode="aspectFill"></image>
+			<view class="mask">美女明星</view>
+			<view class="tap">3天前更新</view>
+		</navigator>
+		<navigator url="#" class="box more" v-else>
+			<image class="pic" src="../../common/image/swapper2.png" mode="aspectFill"></image>
+			<view class="mask">
+				<uni-icons type="more-filled" size="34" color="#fff"></uni-icons>
+				<text>更多</text>
+			</view>
+		</navigator>
+	</view>
+</template>
+
+<script setup>
+	defineProps({
+		isMore:{
+			type: Boolean,
+			default: false
+		}
+	})
+</script>
+
+<style lang="scss" scoped>
+.themeItem {
+	.box {
+		height: 340rpx;
+		border-radius: 10rpx;
+		overflow: hidden;
+		position: relative;
+		.pic {
+			width: 100%;
+			// height: 600%;
+		}
+		.mask {
+			width: 100%;
+			height: 80rpx;
+			position: absolute;
+			bottom: 0;
+			lef: 0;
+			background: rgba(0, 0, 0, 0.2);
+			color: #fff;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			backdrop-filter: blur(20rpx);
+			font-weight: 600;
+			font-size: 30rpx
+		}
+		.tap {
+			position: absolute;
+			left: 0;
+			top: 0rpx;
+			background: rgba(250, 129, 90, 0.7);
+			backdrop-filter: blur(20rpx);
+			font-style: 22rpx;
+			padding: 6px 10px;
+			border-radius: 0 0 20rpx 0;
+			transform: scale(0.8);
+			transform-origin: left top;
+		}
+	}
+	/* 既有box 又有more */
+	.box.more {
+		.mask {
+			width: 100%;
+			height: 100%;
+			flex-direction: column;
+		}
+		.text {
+			font-size: 28rpx;
+		}
+	}
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,59 @@
 <template>
 	<view class="homeLayout">
-		
+		<view class="banner">
+			<swiper indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay circular>
+				<swiper-item v-for="item in 3">
+					<image src="../../common/image/swapper1.png" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+				<text>公告</text>
+			</view>
+			<view class="center">
+				<swiper vertical autoplay interval="1500" duration="300" circular>
+					<swiper-item v-for="item in 4">文字内容</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="right" size="20" color="#333"></uni-icons>
+			</view>
+		</view>
+		<view class="select">
+			<common-title>
+				<template #name>每日推荐</template>
+				<template #custom>
+					<view class="data">
+						<uni-icons type="calendar" size="20" color="#28b389"></uni-icons>
+						<view class="text">
+							<uni-dateformat :date="Date.now()" format="dd号"></uni-dateformat>
+						</view>
+					</view>
+				</template>
+			</common-title>
+			<view class="content">
+				<scroll-view scroll-x>
+					<view class="box" v-for="item in 8">
+						<image src="../../common/image/scroll.png" mode="aspectFill"></image>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+		<view class="theme">
+			<common-title>
+				<template #name>专题精选</template>
+				<template #custom>
+					<navigator url="" class="more">More+</navigator>
+				</template>
+			</common-title>
+			
+			<view class="content">
+				<theme-item v-for="item in 8"></theme-item>
+				<theme-item :isMore="true"></theme-item>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -9,8 +62,110 @@
 </script>
 
 <style lang="scss" scoped>
-	.homeLayout{
-		
+	.homeLayout {
+		.banner {
+			width: 750rpx;
+			padding: 20rpx 0;
+			swiper{
+				width: 750rpx;
+				&-item {
+					width: 100%;
+					height: 100%;
+					padding: 0 20rpx;
+					image {
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+			}
+		}
+		.notice {
+			width: 690rpx;
+			height: 80rpx;
+			background: #f9f9f9;
+			margin: 0 auto;
+			border-radius: 80rpx;
+			display: flex;
+			line-height: 80rpx;
+			.left {
+				width: 140rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				.text {
+					color: #28b389;
+					font-weight: 600;
+					font-size: 28rpx
+				}
+			}
+			.center {
+				flex: 1;
+				swiper {
+					height: 100%;
+					&-item {
+						height: 100%;
+						font-size: 30rpx;
+						color: #666;
+						overflow: hidden;
+						white-space: nowrap;
+						text-overflow: ellipsis;
+					}
+				}
+			}
+			.right {
+				width: 70rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+		}
+		.select {
+			padding-top: 50rpx;
+			.content {
+				width: 720rpx;
+				margin-left: 30rpx;
+				margin-top: 30rpx;
+			}
+			.data {
+				color: #28b389;
+				display: flex;
+				align-items: center;
+				.text {
+					margin-left: 5rpx;
+				}
+			}
+			scroll-view {
+				white-space: nowrap;
+				.box {
+					width: 200rpx;
+					height: 430rpx;
+					display: inline-block;
+					margin-right: 15rpx;
+					image {
+						width: 100%;
+						height: 100%;
+					}
+				}
+				.box:last-child {
+					margin-right: 30rpx;
+				}
+			}
+		}
+		.theme{
+			padding: 50rpx 0;
+			.more {
+				font-size: 32rpx;
+				color: #888;
+			}
+			.content {
+				margin-top: 30rpx;
+				padding: 0 30rpx ;
+				display: grid;
+				gap: 15rpx;
+				grid-template-columns: repeat(3,1fr);
+			}
+		}
 	}
 	
 </style>
